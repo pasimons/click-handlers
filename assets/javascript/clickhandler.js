03acbc59556ae784bb29ab5288425fa87668fe86
pasimons <62959832+pasimons@users.noreply.github.com>
2020-05-08 19:18:44 -0400

    Update clickhandler.js

--- assets/javascript/clickhandler.js
+++ assets/javascript/clickhandler.js
@@ -1,60 +1,77 @@
-const color_box = document.querySelector("#color_box");
-// JS for the gray button
-const gray_button = document.querySelector("#gray_button");
-gray_button.addEventListener("click", function () {
-    color_box.className = "gray";
-})
-// JS for the blue button
-const blue_button = document.querySelector("#blue_button");
-blue_button.addEventListener("click", function () {
-    color_box.className = "blue";
-})
-
-const pink_button = document.querySelector("#pink_button");
-pink_button.addEventListener("click", function () {
-    color_box.className = "pink";
-})
-/* TODO 1
-*
-* Add the JS for the pink button
-*
-*/
-let count = 0;
-const countspan = document.querySelector("#countspan");
-const counter_button = document.querySelector("#counter_button");
-counter_button.addEventListener("click", function () {
-
-    console.log ("+ button clicked")
-
-    let newValue = parseInt(countspan.innerHTML) +1;
-
-    countspan.innerHTML=newValue;
+let incrementButton = document.querySelector("#increment"); 
+let decrementButton = document.querySelector("#decrement"); 
+let counter = document.querySelector("#counter"); 
 
 
-    /* TODO 2
-    *
-    * Fill in this function so that it increments
-    * (adds one to) the variable named count,
-    * then updates the inner text of "countspan"
-    * to show the current value of "count".
-    */
-})
-const even_or_odd_button = document.querySelector("#even_or_odd_button");
-even_or_odd_button.addEventListener("click", function () {
 
-    if (num % 2==0)
+// This line says, "I want to listen for someone to CLICK my
+// incrementButton, and any time I 'hear' that event occur,
+// I want some code to run.
+incrementButton.addEventListener("click", function() {
+    // Any code you write in here, will run every time incrementButton
+    // is clicked.
+    // Run the console.log function to display some text in the console:
+    console.log("+ button clicked");
 
-    {
-        
-    }
+    // Calculate the new value for our counter:
+  let newCounterValue = Number(counter.innerHTML) + 1;
+
+  // Calculate the new value for our counter:
+  counter.innerHTML = newCounterValue;
+  }) // We have to close the curly braces and parentheses we opened above.
 
 
 
 
-    /* TODO 3
-    *
-    * Fill in this function so that it shows an
-    * alert dialog stating whether the count variable
-    * (from part 2, above) is even or odd.
-    */
-})
+
+
+  decrementButton.addEventListener("click", function() {
+    // Any code you write in here, will run every time incrementButton
+    // is clicked.
+    // Run the console.log function to display some text in the console:
+    console.log("- button clicked");
+
+    // Calculate the new value for our counter:
+  let newCounterValue = Number(counter.innerHTML) - 1;
+
+  // Calculate the new value for our counter:
+  counter.innerHTML = newCounterValue;
+  }) // We have to close the curly braces and parentheses we opened above.let incrementButton = document.querySelector("#increment"); 
+let decrementButton = document.querySelector("#decrement"); 
+let counter = document.querySelector("#counter"); 
+
+
+
+// This line says, "I want to listen for someone to CLICK my
+// incrementButton, and any time I 'hear' that event occur,
+// I want some code to run.
+incrementButton.addEventListener("click", function() {
+    // Any code you write in here, will run every time incrementButton
+    // is clicked.
+    // Run the console.log function to display some text in the console:
+    console.log("+ button clicked");
+
+    // Calculate the new value for our counter:
+  let newCounterValue = Number(counter.innerHTML) + 1;
+
+  // Calculate the new value for our counter:
+  counter.innerHTML = newCounterValue;
+  }) // We have to close the curly braces and parentheses we opened above.
+
+
+
+
+
+
+  decrementButton.addEventListener("click", function() {
+    // Any code you write in here, will run every time incrementButton
+    // is clicked.
+    // Run the console.log function to display some text in the console:
+    console.log("- button clicked");
+
+    // Calculate the new value for our counter:
+  let newCounterValue = Number(counter.innerHTML) - 1;
+
+  // Calculate the new value for our counter:
+  counter.innerHTML = newCounterValue;
+  }) // We have to close the curly braces and parentheses we opened above.
